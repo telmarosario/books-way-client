6d9b8ae63621f6cf4d77ae66e906ecc9c0775b15
Telma Rosário <telmarrosario@gmail.com>
2021-12-12 13:09:37 +0000

    Display information on profile page

--- src/pages/OtherUser/OtherUser.js
+++ src/pages/OtherUser/OtherUser.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import userService from "../../services/user.service";
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,15 +1,56 @@
-import { useContext } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth.context";
+import userService from "../../services/user.service";
+import { Link } from "react-router-dom";
 
 function ProfilePage() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const [userInfo, setUserInfo] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const userId = user._id;
+
+  const getUserInfo = async () => {
+    try {
+      const response = await userService.getUsersProfile(userId);
+      setUserInfo(response.data);
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   return (
     <div>
       <h1>Profile Page</h1>
+      {errorMessage && <p>{errorMessage}</p>}
       {isLoggedIn && (
         <div>
           <button onClick={logOutUser}>Logout</button>
+        </div>
+      )}
+
+      {userInfo && (
+        <div>
+          <img src={userInfo.imageUrl} alt="profile" />
+          <h4>{userInfo.username}</h4>
+          <h4>{userInfo.email}</h4>
+          {userInfo.favoriteGenres.map((genre) => {
+            return <span>{genre} </span>;
+          })}
+
+          {userInfo.booksSaleTrade.map((book) => {
+            return (
+              <Link to={`/books/${book._id}`}>
+                <h4>{book.title}</h4>
+                <img src={book.imageUrl} alt="" />
+              </Link>
+            );
+          })}
         </div>
       )}
     </div>
